e93da4f48ea012982dda864a2516b9fb95f8b836
Carrie Liang <liangcarrie20@gmail.com>
2016-04-16 13:33:51 -0700

    set age and birth year variables to integer

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,9 @@
 puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "What year were you born?"
-birthyear = gets.chomp
+birthyear = gets.chomp.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
