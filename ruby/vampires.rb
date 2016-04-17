1cde375b9eb4fea01862a4f0c7ed0438acd62814
Carrie Liang <liangcarrie20@gmail.com>
2016-04-16 22:33:41 -0700

    added until loop to test multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,54 @@
 #Vampire Detector
-
-puts "What is your name?"
-name = gets.chomp
-
-puts "How old are you?"
-age = gets.chomp.to_i
-
-puts "What year were you born?"
-birthyear = gets.chomp.to_i
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic = gets.chomp
-
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-insurance = gets.chomp
-
 current_year = 2016
+num_employees = 0
 
-#Even if the employee is an amazing liar otherwise,
-#anyone going by the name of “Drake Cula” or “Tu Fang”
-#is clearly a vampire, because come on. In that case,
-#you should print “Definitely a vampire.”
-if name == "Drake Cula"
-	puts "Definitely a vampire."
-elsif name == "Tu Fang"
-	puts "Definitely a vampire."
-#If the employee got their age right,
-#and is willing to eat garlic bread or sign up for insurance,
-#the result is “Probably not a vampire.”
-elsif (current_year - birthyear == age) && (garlic == "y" || insurance == "y")
-	puts "Probably not a vampire."
+puts "How many employees will be processed today?"
+num_employees = gets.chomp.to_i
 
-#If the employee got their age wrong,
-#hates garlic bread, and doesn’t want insurance,
-#the result is “Almost certainly a vampire.”
-elsif (current_year - birthyear != age) && (garlic == "n" && insurance == "n")
-	puts "Almost certainly a vampire."
+interviews = 0
+until interviews == num_employees
+	puts "What is your name?"
+	name = gets.chomp
 
-#If the employee got their age wrong,
-#and hates garlic bread or waives insurance,
-#the result is “Probably a vampire.”
-elsif (current_year - birthyear != age) && (garlic == "n" || insurance == "n")
-	puts "Probably a vampire."
-else
-	puts "Results inconclusive."
+	puts "How old are you?"
+	age = gets.chomp.to_i
+
+	puts "What year were you born?"
+	birthyear = gets.chomp.to_i
+
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	garlic = gets.chomp
+
+	puts "Would you like to enroll in the company's health insurance? (y/n)"
+	insurance = gets.chomp
+
+	#Even if the employee is an amazing liar otherwise,
+	#anyone going by the name of “Drake Cula” or “Tu Fang”
+	#is clearly a vampire, because come on. In that case,
+	#you should print “Definitely a vampire.”
+	if name == "Drake Cula"
+		puts "Definitely a vampire."
+	elsif name == "Tu Fang"
+		puts "Definitely a vampire."
+	#If the employee got their age right,
+	#and is willing to eat garlic bread or sign up for insurance,
+	#the result is “Probably not a vampire.”
+	elsif (current_year - birthyear == age) && (garlic == "y" || insurance == "y")
+		puts "Probably not a vampire."
+
+	#If the employee got their age wrong,
+	#hates garlic bread, and doesn’t want insurance,
+	#the result is “Almost certainly a vampire.”
+	elsif (current_year - birthyear != age) && (garlic == "n" && insurance == "n")
+		puts "Almost certainly a vampire."
+
+	#If the employee got their age wrong,
+	#and hates garlic bread or waives insurance,
+	#the result is “Probably a vampire.”
+	elsif (current_year - birthyear != age) && (garlic == "n" || insurance == "n")
+		puts "Probably a vampire."
+	else
+			puts "Results inconclusive."
+			interviews += 1
+	end
 end
