86fa7662d66d0ac7c821b26850a3381092f239c1
Carrie Liang <liangcarrie20@gmail.com>
2016-04-17 02:05:48 -0700

    added basic method structure

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,13 @@
+# encrypt method
+# INPUT: password
+# OUTPUT: password that's advanced one letter forward for each letter
+# example inputs: abc, zed, bcd, afe (STRING)
+# example outputs: bcd, afe, abc, zed (STRING)
+
+#define method
+def encrypt
+	#do something
+	puts "enter your password"
+end
+encrypt
+encrypt
