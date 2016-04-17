d393220fb42e7dca8498eb8329a6607ab14e2ca7
Carrie Liang <liangcarrie20@gmail.com>
2016-04-17 16:21:16 -0700

    revisted pseudo code, added if statement for z.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,14 +1,38 @@
 # encrypt method
 # INPUT: password
+# get length of password to put in variable
 # OUTPUT: password that's advanced one letter forward for each letter
+	#grab letter
+	#increment letter
+	#add to convert
 # example inputs: abc, zed (STRING)
 # example outputs: bcd, afe (STRING)
 
 #define method
 def encrypt (password)
-	#index each letter and use next
-	password[0].next + password[1].next + password[2].next
+	i = 0
+	convert = ""
+	length = password.length
+
+	for i in 0...length
+		convert += password[i].next
+		if password[i] == "z"
+			convert = convert.chomp("a")
+		end
+	end
+	return convert
 end
 
 puts encrypt("abc")
 puts encrypt("zed")
+
+#decrypt method
+#read password string
+#get length of password to put in variable
+#OUTPUT: decrypted password (each letter is brought back one letter)
+	#get letter
+	#get index number
+	#decrese index number by one
+	#translate back into alphabet string
+	#add to convert
+
