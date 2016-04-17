f36c25c8695b559b3fc00b7867b0d6583d8d9b03
Carrie Liang <liangcarrie20@gmail.com>
2016-04-16 19:10:26 -0700

    added if statements to detect vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,5 @@
+#Vampire Detector
+
 puts "What is your name?"
 name = gets.chomp
 
@@ -7,30 +9,40 @@ age = gets.chomp.to_i
 puts "What year were you born?"
 birthyear = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance = gets.chomp
 
 current_year = 2016
 
-if age == (current_year - birthyear) && (garlic == "yes" && insurance == "yes")
+#If the employee got their age right,
+#and is willing to eat garlic bread or sign up for insurance,
+#the result is “Probably not a vampire.”
+if (current_year - birthyear == age) && (garlic == "y" || insurance == "y")
 	puts "Probably not a vampire."
-end
 
-if !age == (current_year - birthyear) && (garlic == "no" || insurance == "no")
+#If the employee got their age wrong,
+#and hates garlic bread or waives insurance,
+#the result is “Probably a vampire.”
+elsif !(current_year - birthyear == age) && (garlic == "n" || insurance == "n")
 	puts "Probably a vampire."
-end
 
-if !age == (current_year - birthyear) && (garlic == "no" && insurance == "no")
+#If the employee got their age wrong,
+#hates garlic bread, and doesn’t want insurance,
+#the result is “Almost certainly a vampire.”
+elsif !(current_year - birthyear == age) && (garlic == "n" && insurance == "n")
 	puts "Almost certainly a vampire."
-end
 
-if name == "Drake Cula"
+#Even if the employee is an amazing liar otherwise,
+#anyone going by the name of “Drake Cula” or “Tu Fang”
+#is clearly a vampire, because come on. In that case,
+#you should print “Definitely a vampire.”
+elsif name == "Drake Cula"
 	puts "Definitely a vampire."
-end
-
-if name == "Tu Fang"
+elsif name == "Tu Fang"
 	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
 end
