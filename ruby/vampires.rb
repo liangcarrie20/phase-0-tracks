519460385735114a78cde9e352c682e53c6f5502
Carrie Liang <liangcarrie20@gmail.com>
2016-04-16 23:57:53 -0700

    moved interviews += 1 outside of if/elsif/else statments

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,6 +7,7 @@ num_employees = gets.chomp.to_i
 
 interviews = 0
 until interviews == num_employees
+
 	puts "What is your name?"
 	name = gets.chomp
 
@@ -20,35 +21,23 @@ until interviews == num_employees
 	garlic = gets.chomp
 
 	puts "Would you like to enroll in the company's health insurance? (y/n)"
-	insurance = gets.chomp
+	insurance = gets.chomp			
 
-	#Even if the employee is an amazing liar otherwise,
-	#anyone going by the name of “Drake Cula” or “Tu Fang”
-	#is clearly a vampire, because come on. In that case,
-	#you should print “Definitely a vampire.”
-	if name == "Drake Cula"
-		puts "Definitely a vampire."
-	elsif name == "Tu Fang"
-		puts "Definitely a vampire."
-	#If the employee got their age right,
-	#and is willing to eat garlic bread or sign up for insurance,
-	#the result is “Probably not a vampire.”
-	elsif (current_year - birthyear == age) && (garlic == "y" || insurance == "y")
-		puts "Probably not a vampire."
+		if name == "Drake Cula"
+			puts "Definitely a vampire."
+		elsif name == "Tu Fang"
+			puts "Definitely a vampire."
 
-	#If the employee got their age wrong,
-	#hates garlic bread, and doesn’t want insurance,
-	#the result is “Almost certainly a vampire.”
-	elsif (current_year - birthyear != age) && (garlic == "n" && insurance == "n")
-		puts "Almost certainly a vampire."
+		elsif (current_year - birthyear == age) && (garlic == "y" || insurance == "y")
+			puts "Probably not a vampire."
 
-	#If the employee got their age wrong,
-	#and hates garlic bread or waives insurance,
-	#the result is “Probably a vampire.”
-	elsif (current_year - birthyear != age) && (garlic == "n" || insurance == "n")
-		puts "Probably a vampire."
-	else
-			puts "Results inconclusive."
-			interviews += 1
-	end
+		elsif (current_year - birthyear != age) && (garlic == "n" && insurance == "n")
+			puts "Almost certainly a vampire."
+
+		elsif (current_year - birthyear != age) && (garlic == "n" || insurance == "n")
+			puts "Probably a vampire."
+		else
+				puts "Results inconclusive."
+		end
+	interviews += 1
 end
