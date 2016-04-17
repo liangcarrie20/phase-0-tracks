e834a2701d1fa3429ac2d2bb08624ccbe6e2b987
Carrie Liang <liangcarrie20@gmail.com>
2016-04-17 16:56:13 -0700

    added prompts to ask user to enter password an whether they'd like decryption or encryption

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -65,8 +65,14 @@ p decrypt(encrypt("swordfish"))
 # The nested method for this call workds because it is assessed from right to left, filling in the variables as it moves left.
 
 #Driver code
+
 #ask user to enter password
+puts "Enter your password."
+password = gets.chomp
+
 #ask user if they'd like password encrypted or decrypted
+puts "Would you like to have your password encrypted or decrypted?"
+response = gets.chomp
 
 #either encrypt or decrypt password
 #print password
