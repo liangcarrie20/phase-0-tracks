d4512bb981eeaab0dc858140370b40bccbb34cf9
Carrie Liang <liangcarrie20@gmail.com>
2016-04-19 23:07:05 -0700

    added .gsub method to replace excess whitespace and replacing it with only one whitespace

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -28,7 +28,7 @@ puts "The case of the disappearing last letter".chop
 puts "The mystery of the missing first letter".slice(1..-1)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".gsub(/\s+/, " ")
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
