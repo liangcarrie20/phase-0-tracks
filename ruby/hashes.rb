ccea83d5cbc2e4c257d881553e419784e43b11e8
Carrie Liang <liangcarrie20@gmail.com>
2016-04-20 23:51:00 -0700

    added pseudocode for interior design client questions

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+# Ask interior designer to enter client's name (string)
+# Ask interior designer to enter client's age (integer)
+	# Convert input to integer
+# Ask interior designer to enter client's number of children (integer)
+	# Convert input to integer
+# Ask interior designer to enter client's decor theme (string)
+# Ask interior designer to enter whether or not client likes chandeliers (boolean)
+	# Convert input to boolean ('yes'.to_b, 'y'.to_b)
+# Ask interior designer to enter whether or not client likes lime green paint (boolean)
+	# Convert input to boolean ('yes'.to_b, 'y'.to_b)
+# Ask interior designer to enter whether or not client likes purple (boolean)
+	# Convert input to boolean ('yes'.to_b, 'y'.to_b)
+# Ask interior designer to enter whether or not client likes red (boolean)
+	# Convert input to boolean ('yes'.to_b, 'y'.to_b)
+# Print hash when interior designer completed all questions
+# Ask interior designer if she needs to update key (no need to loop); if 'none', skip...
+# Print latest version of hash for the designer
