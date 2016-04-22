3563164912acc3d9293b728f85c8228af1762750
Carrie Liang <liangcarrie20@gmail.com>
2016-04-22 16:55:02 -0700

    designed a nested data structure for shopping mall

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+shopping_mall {
+	'First Floor' => [
+		'beard_grandpas',
+		'dragon_express',
+		'jumbo_juice'
+	],
+	'Second Floor' => [
+		'old_violet',
+		'the_cap',
+		'apple_republic'
+	],
+	'Thrid Floor' => [
+		'lacys',
+		'mordstroms',
+		'saks_sixth_ave'
+	]
+}
