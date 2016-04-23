d2237c36b296e9709f5f2cdd5569a87d2b20b8d4
Carrie Liang <liangcarrie20@gmail.com>
2016-04-22 17:10:34 -0700

    updated nested data structure for shopping mall

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,17 +1,41 @@
-shopping_mall {
-	'First Floor' => [
-		'beard_grandpas',
-		'dragon_express',
-		'jumbo_juice'
-	],
-	'Second Floor' => [
-		'old_violet',
-		'the_cap',
-		'apple_republic'
-	],
-	'Thrid Floor' => [
-		'lacys',
-		'mordstroms',
-		'saks_sixth_ave'
-	]
+shopping mall = {
+	first_floor: {
+		category: 'food',
+		store_directory: [
+			'beard_grandpas',
+			'jumbo_juice',
+			'dragon_express'
+		]
+		unit_info: {
+			total_units: 6,
+			units_available: 3
+		}
+	},
+	second_floor: {
+		category: 'apparel',
+		store_directory: [
+			'old_violet',
+			'the cap',
+			'apple_republic',
+			'lacys',
+			'saks_sixth_ave'
+		]
+		unit_info: {
+			total_units: 8,
+			units_available: 3
+		}
+	}
+	third_floor: {
+		category: 'shoes',
+		store_directory [
+			'foot_locker_room',
+			'paymore_shoe_source',
+			'conversely',
+			'UGG_New_Zealand',
+		]
+		unit_info: {
+			total_units: 9,
+			units_available: 5
+		}
+	}
 }
