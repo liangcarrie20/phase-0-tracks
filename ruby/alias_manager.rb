a40f39d9973e695200d447930e7c6da9cc31d2e0
Carrie Liang <liangcarrie20@gmail.com>
2016-04-22 18:20:35 -0700

    completed pseudocode for secret agents

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,6 @@
+# ask agent for their real name
+# set name variable equal to a string of the agent's name using gets.chomp
+# change first and last name string into an array of words using split(' ')
+# reverse the array
+# change vowels to the next vowel (convert "u" to "a")
+# change consonants to the next consonant (convert "z" to "b")
