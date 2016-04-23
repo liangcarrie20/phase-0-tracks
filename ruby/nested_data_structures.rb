f467fd09f53dc82f7077afe5f149f06fc02345e4
Carrie Liang <liangcarrie20@gmail.com>
2016-04-22 17:25:54 -0700

    fixed syntax errors in data structure; accessed category of first floor

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,11 +1,12 @@
-shopping mall = {
+# Build nested structure for shopping mall
+shopping_mall = {
 	first_floor: {
 		category: 'food',
 		store_directory: [
 			'beard_grandpas',
 			'jumbo_juice',
 			'dragon_express'
-		]
+		],
 		unit_info: {
 			total_units: 6,
 			units_available: 3
@@ -19,23 +20,26 @@ shopping mall = {
 			'apple_republic',
 			'lacys',
 			'saks_sixth_ave'
-		]
+		],
 		unit_info: {
 			total_units: 8,
 			units_available: 3
 		}
-	}
+	},
 	third_floor: {
 		category: 'shoes',
-		store_directory [
+		store_directory: [
 			'foot_locker_room',
 			'paymore_shoe_source',
 			'conversely',
 			'UGG_New_Zealand',
-		]
+		],
 		unit_info: {
 			total_units: 9,
 			units_available: 5
 		}
 	}
 }
+
+# Access category of first floor:
+p shopping_mall[:first_floor][:category]
