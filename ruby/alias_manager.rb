c20c5d5b85d55aabe08ba3e936182ad5e81d2163
Carrie Liang <liangcarrie20@gmail.com>
2016-04-22 18:26:21 -0700

    asked user to input name, turned first and last name into array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,11 @@
 # ask agent for their real name
 # set name variable equal to a string of the agent's name using gets.chomp
 # change first and last name string into an array of words using split(' ')
+puts "What is your first and last name?"
+name = gets.chomp.split
+p name
+
+
 # reverse the array
 # change vowels to the next vowel (convert "u" to "a")
 # change consonants to the next consonant (convert "z" to "b")
