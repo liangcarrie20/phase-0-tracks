acdb4cb9b4845e3daa7c5064896f1dda6d7634e2
Carrie Liang <liangcarrie20@gmail.com>
2016-04-22 23:12:11 -0700

    combined steps into one line

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,14 @@
 # ask agent for their real name
 # set name variable equal to a string of the agent's name using gets.chomp
 # change first and last name string into an array of words using split(' ')
-puts "What is your first and last name?"
-name = gets.chomp.split
-p name
-
 # reverse the array
-name.reverse!
+# convert back to string
+puts "What is your first and last name?"
+name = gets.chomp.split.reverse!.join(' ')
 p name
 
-# change vowels to the next vowel (convert "u" to "a")
+# method to change vowels to the next vowel (convert "u" to "a")
+
+
 # change consonants to the next consonant (convert "z" to "b")
 # define method of encryption
