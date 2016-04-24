cbbb13fe7f82bbc6f9e2cd12669a068a2e4dd711
Carrie Liang <liangcarrie20@gmail.com>
2016-04-23 22:54:51 -0700

    added iteration with map! to replace letters

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,6 +4,16 @@
 # define method for: 
 # Ask user to enter first and last name, making input downcase (string)
 # Turn user's first and last name into an array
+# Swap first and last name
+	# Reverse the order of the array
+
+# change vowels to next vowel
+# change consonants to next consonant
+	# iterate through name changing using if/elsif/else
+	# a to e, e to i, i to o, o to u, u to a
+	#  b to d, d to f, h to j, n to p, t to v, z to b
+	#  " " to " "
+	# else letter.next
 def spy_alias (first_name, last_name)
 	full_name = []
 	puts "What is your first name?"
@@ -15,21 +25,37 @@ def spy_alias (first_name, last_name)
 	full_name.reverse!
 
 	until first_name == "quit"
-		spy_name = full_name.join(' ')
-		name_string = full_name.split('')
-	end
+		spy_name = full_name.join(' ') #turn name back into string
+		name_string = full_name.split('') #split string to array of letters"
 
+		name_string.map! do |letter|
+			if letter == "a"
+				letter == "e"
+			elsif letter == "e"
+				letter == "i"
+			elsif letter == "i"
+				letter == "o"
+			elsif letter == "o"
+				letter == "u"
+			elsif letter == "u"
+				letter == "a"
+			elsif letter == "b"
+				letter == "d"
+			elsif letter == "d"
+				letter == "f"
+			elsif letter == "h"
+				letter == "j"
+			elsif letter == "n"
+				letter == "p"
+			elsif letter == "t"
+				letter == "v"
+			elsif letter == "z"
+				letter == "b"
+			else
+				letter.next
+			end
+		end
+	end
 end
 
-# Swap first and last name
-	# Reverse the order of the array
-
-# change vowels to next vowel
-# change consonants to next consonant
-	# iterate through name changing using if/elsif/else
-	# a to e, e to i, i to o, o to u, u to a
-	#  b to d, d to f, h to j, n to p, t to v, z to b
-	#  " " to " "
-	# else letter.next
-
-# print out secret agent name and capitalize
+# print out secret agent name and change case
