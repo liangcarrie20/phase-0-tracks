a85bd1447e242940de506303c7f3ab62bc758ff7
Carrie Liang <liangcarrie20@gmail.com>
2016-04-27 19:21:47 -0700

    added diverse initializations of santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -28,6 +28,14 @@ class Santa
 end
 
 # check that santa can be initialized
-kris = Santa.new
+kris = Santa.new("female", "North Polean")
 kris.speak
 kris.eat_milk_and_cookies("peanut butter cookie")
+
+# diverse initializations
+santas = []
+example_genders = ["female", "male", "bigender", "N/A", "gender variant", "female", "two-spirit"]
+example_ethnicities = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+example_genders.length.times do |i|
+	santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
