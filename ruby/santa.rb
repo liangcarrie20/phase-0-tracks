6a2d3fa0e6c3d7239e304ea6413372f1d8e1ccae
Carrie Liang <liangcarrie20@gmail.com>
2016-04-27 17:05:19 -0700

    defined initialize, speak, eat milke and cookies methods.  tested that santa can be initialized, speak, and eat milk and cookies

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,24 @@
 # Define santa class
 
 class Santa
+
+	# define initialize method
+	def initialize
+		puts "Initializing Santa instance ..."
+
+	# define speak method
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	# define eat milk and cookies method
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end
+
 end
+
+# check that santa can be initialized
+kris = Santa.new
+kris.speak
+kris.eat_milk_and_cookies("peanut butter cookie")
