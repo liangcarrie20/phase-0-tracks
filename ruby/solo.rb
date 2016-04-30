128281c0707a101da6452e83c303e03848a58723
Carrie Liang <liangcarrie20@gmail.com>
2016-04-30 14:51:32 -0700

    defined initialize method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -4,6 +4,12 @@ class Cat
 	# default age 0 years old
 	# breeds: Bengal, Manx, Munchkin, Persian, Siamese
 	# personality: cuddly, feisty, independent
+	def initialize(breed, personality)
+		puts "Initilizing Cat instance ..."
+		@breed = breed
+		@personality = personality
+		@age = 0
+	end
 # define method to make Cat meow
 # define method to make Cat scratch
 # define method to make Cat eat(food)
