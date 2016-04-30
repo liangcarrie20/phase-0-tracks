d3bf4047eb750f9065a51fdd38184994358edd7b
Carrie Liang <liangcarrie20@gmail.com>
2016-04-30 15:21:07 -0700

    added name attr, made breed and personality readable, name and gender writable

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,31 +1,37 @@
 # Create Cat class
 class Cat
-# define method to initialize an instance of Cat
-	# default age 0 years old
-	# breeds: Bengal, Manx, Munchkin, Persian, Siamese
-	# personality: cuddly, feisty, independent
-	def initialize(breed, personality)
+	# use attr_reader and attr_accessor
+	attr_reader :breed, :personality
+	attr_accessor :name, :gender
+
+	# define method to initialize an instance of Cat
+		# name
+		# default age 0 years old
+		# breeds: Bengal, Manx, Munchkin, Persian, Siamese
+		# personality: cuddly, feisty, independent
+	def initialize(name, breed, personality)
 		puts "Initilizing Cat instance ..."
+		@name = name
 		@breed = breed
 		@personality = personality
 		@age = 0
 	end
-# define method to make Cat meow
+	# define method to make Cat meow
 	def speak(num)
 		puts "Meow " * num
 	end
-# define method to make Cat scratch
+	# define method to make Cat scratch
 	def scratch(item)
 		puts "Cat scratched #{item}"
 	end
-# define method to make Cat eat(food)
+	# define method to make Cat eat(food)
 	def eat(food)
 		puts "Cat ate #{food}"
 	end
 end
 
 # Verify Cat can be initialized
-cat = Cat.new("Bengal", "cuddly")
+cat = Cat.new("Marlon", "Bengal", "cuddly")
 cat.speak(2)
 cat.scratch("sofa")
 cat.eat("fish")
