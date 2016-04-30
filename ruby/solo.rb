9f1502542f258d89f0135287bd2f860635b55f0e
Carrie Liang <liangcarrie20@gmail.com>
2016-04-30 14:48:42 -0700

    designed Cat method

---
+++ ruby/solo.rb
@@ -0,0 +1,8 @@
+# Create Cat class
+# define method to initialize an instance of Cat
+	# default age 0 years old
+	# breeds: Bengal, Manx, Munchkin, Persian, Siamese
+	# personality: cuddly, feisty, independent
+# define method to make Cat meow
+# define method to make Cat scratch
+# define method to make Cat eat(food)
