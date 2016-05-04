2ee0c4dd885f10fa251d550fd0d12a75e3a5affa
Carrie Liang <liangcarrie20@gmail.com>
2016-05-04 09:13:38 -0700

    completed pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// define reverse function that takes a string parameter
+// define variable to store reversedString
+// set var i equal to string.length -1
+// while i is greater than or equal to 0
+// decrement i (i--)
+// have string[i] be added to reversedString
+// return reversedString
