9084423c1d5bcb3fe551b4fce1a6b71cb9190e07
Carrie Liang <liangcarrie20@gmail.com>
2016-05-04 19:36:43 -0700

    completed release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,23 @@ for (i=0; i < horse_names.length; i++) {
 };
 
 console.log(horse_hash);
+
+function Car(type, topSpeed, color, functioning) { 
+  this.type = type;
+  this.topSpeed = topSpeed;
+  this.color = color;
+  this.functioning = functioning;
+  this.beep = function() {console.log("Beep!"); };
+}
+
+var carOne = new Car("Prius", 110, "black", true);
+console.log(carOne);
+carOne.beep();
+
+var carTwo = new Car("Tesla", 180, "red", true);
+console.log(carTwo);
+carTwo.beep();
+
+var carThree = new Car("Ford", 160, "blue", true);
+console.log(carThree);
+carThree.beep();
