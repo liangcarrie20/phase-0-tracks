41721e53fc7007508fd7c831214ed1b00d2ffcba
Carrie Liang <liangcarrie20@gmail.com>
2016-05-04 19:31:52 -0700

    added another color to colors array and another name for horse name array

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,7 @@
 var colors = ["Metallic Sunburst","Alien Armpit","Medium Tuscan Red","Sapphire"];
 
 var horse_names = ["Adios","Bullseye","Eclipse","Hurricane Fly"];
+
+colors.push("Spanish Orange");
+
+horse_names.push("Lottery");
