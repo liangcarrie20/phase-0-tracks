62a3bc17d0e82a282c2682c4d0ee1be63b0c9d91
Carrie Liang <liangcarrie20@gmail.com>
2016-05-04 19:35:54 -0700

    created loop that assigns color to horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,15 @@
 var colors = ["Metallic Sunburst","Alien Armpit","Medium Tuscan Red","Sapphire"];
 
-var horse_names = ["Adios","Bullseye","Eclipse","Hurricane Fly"];
+var horse_names = ["Adios","Bullseye","Eclipse","Hurricane_Fly"];
 
 colors.push("Spanish Orange");
 
 horse_names.push("Lottery");
+
+var horse_hash = {};
+
+for (i=0; i < horse_names.length; i++) {
+  horse_hash[horse_names[i]] = colors[i];
+};
+
+console.log(horse_hash);
