ddd84695afefd098bc249f3487e55ca956c0735b
Carrie Liang <liangcarrie20@gmail.com>
2016-05-07 15:00:00 -0700

    created algos.js file; finished pseudocode for function

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+var array = ["long phrase","longest phrase","longer phrase"];
+
+// Function for: FINDING THE LONGEST WORD:
+// loop through to find length of each array value
+// set i = 0 ; use FOR loop
+// new variable named longest
+// if true, variable longest word is equal to index of array
+// return longest
