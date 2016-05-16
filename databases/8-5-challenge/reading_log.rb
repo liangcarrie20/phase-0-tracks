9d26f677e195b3d19a041d6bdc3ea725ce956b12
Carrie Liang <liangcarrie20@gmail.com>
2016-05-16 02:07:03 -0700

    tested program, all methods seem to work

--- databases/8-5-challenge/reading_log.rb
+++ databases/8-5-challenge/reading_log.rb
@@ -49,7 +49,7 @@ def add_new_mo(db)
     new_mo = gets.chomp
     new_mo = new_mo.split(" ")
   end
-  db.execute("INSERT INTO months (month, year) VALUES ('#{new_mo[0]}', #{new_mo[1]}")
+  db.execute("INSERT INTO months (month, year) VALUES ('#{new_mo[0]}', #{new_mo[1]})")
   month_id = db.execute("SELECT id FROM months WHERE month = '#{new_mo[0]}' AND year = #{new_mo[1]}")
   month_id = month_id[0][0]
   return month_id
@@ -102,7 +102,7 @@ def add_rating(month_id, db)
     comments = gets.chomp.to_s
   puts "Enter rating on a scale from 1 to 5: "
     rating = gets.chomp.to_i
-  db.execute("INSERT INTO ratings (month_id, comments, rating) VALUES (#{month_id}, ?, ?)" [comments, rating])
+  db.execute("INSERT INTO ratings (month_id, comments, rating) VALUES (#{month_id}, ?, ?)", [comments, rating])
   puts "Rating added.\n"
 end 
 
@@ -141,10 +141,70 @@ def print_mo_ratings(month_id, db)
   total = 0
   count = 1
   month_ratings.each do |month|
-    puts "#{counter}. #{month[0]} - #{month[1]}"
+    puts "#{count}. #{month[0]} - #{month[1]}"
     total += month[1]
     count += 1
   end
   puts"\nTotal rating: #{total}"
   return total
 end
+
+# UI
+# Welcome user to program
+# Ask user what he/she'd like to do from a list of items
+# Allow user to exit
+# Sub menu
+
+puts "Welcome to your Reading Log!"
+def main_menu(db)
+  while true
+    puts "Enter choice number:\n1. Add month \n2. View Month \n3. Delete Month \n4. View Months \n5. Exit"
+    user_response = gets.chomp.to_i
+    if user_response == 1
+      month_id = add_new_mo(db)
+    elsif user_response == 2
+      month_id = choose_mo_id(db)
+      sub_menu(month_id, db)
+    elsif user_response == 3
+      del_mo(db)
+    elsif user_response == 4
+      print_mo_tbl(db)
+    elsif user_response == 5
+      break
+    end
+    puts "Press enter to continue exiting"
+    gets
+  end
+  puts "Do you really want to exit? (yes/no)"
+  user_response = gets.chomp
+  main_menu(db) if user_response == "no"
+  puts "Bye!"
+end
+
+def sub_menu(month_id, db)
+  puts "\nGathering..." + mo_yr(month_id, db)
+  while true
+    puts "Enter choice number:\n1. Add Book\n2. Add Rating\n3. Delete Book\n4. Delete Rating\n5. Print All Books Read in a Month\n6. Print All Ratings in a Month\n7. Return to Main Menu\n"
+    user_response = gets.chomp.to_i
+    puts
+    if user_response == 1
+      add_book(month_id, db)
+    elsif user_response == 2
+      add_rating(month_id, db)
+    elsif user_response == 3
+      del_mo_books(month_id, db)
+    elsif user_response == 4
+      del_mo_rating(month_id, db)
+    elsif user_response == 5
+      print_mo_books(month_id, db)
+    elsif user_response == 6
+      print_mo_ratings(month_id, db)
+    elsif user_response == 7
+      break
+    end
+    puts "Press enter to continue exiting"
+    gets
+  end
+end
+
+main_menu(db)
